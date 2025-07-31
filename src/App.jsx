af41aaaaa1ce6a4f7b82d4c3051ea51ef28383d8
urkiddennn <154726035+urkiddennn@users.noreply.github.com>
2025-07-31 16:57:04 +0800

    Update: added footer section

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import ChatModal from "./components/ChatModal";
 import { getTotalJoined } from "./services/api";
 import Header from "./components/Header";
 import FeatureSection from "./sections/FeatureSection";
+import { IoIosBookmarks } from "react-icons/io";
 
 const App = () => {
   const [totalJoined, setTotalJoined] = useState(0);
@@ -69,7 +70,7 @@ const App = () => {
       {/* Navigation Bar */}
       <Header />
       {/* Main Content Area - Waitlist Focused */}
-      <main className="relative z-10 flex flex-col items-center justify-center flex-grow text-center px-4 py-16">
+      <main className="relative max-w-5xl z-10 flex flex-col items-center justify-center flex-grow text-center px-4 py-16">
         <h1 className="lg:text-5xl text-3xl md:text-4xl font-extrabold text-gray-950 leading-tight mb-6 max-w-4xl">
           Your dedicated partner in mastering the{" "}
           <span className="text-blue-500 hover:text-pink-400 transition duration-200">
@@ -116,18 +117,54 @@ const App = () => {
       </main>
       {/* The feature sections */}
       <FeatureSection />
-      <div className="max-w-4xl gap-4 flex justify-center items-center  bottom-10 opacity-60">
-        <button className="bg-white shadow-xl rounded-full p-3 hover:transform hover:scale-110 hover:bg-gray-100 hover:shadow-2xl transition duration-200 ease-in-out">
-          <FaFacebookF className="size-6 text-gray-500 hover:text-blue-600" />
-        </button>
-        <button className="bg-white shadow-xl rounded-full p-3 hover:transform hover:scale-110 hover:bg-gray-100 hover:shadow-2xl transition duration-200 ease-in-out">
-          <FaXTwitter className="size-6 text-gray-500 hover:text-black" />
-        </button>
-        <button className="bg-white shadow-xl rounded-full p-3 hover:transform hover:scale-110 hover:bg-gray-100 hover:shadow-2xl transition duration-200 ease-in-out">
-          <FaInstagram className="size-6 text-gray-500 hover:text-pink-500" />
-        </button>
-      </div>
-      <footer className="text-gray-300 mt-4">@CivilPrep 2025</footer>
+      <footer className=" relative max-w-5xl footer sm:footer-horizontal bg-gray-950 rounded-t-xl mt-10  text-neutral-content p-10">
+        <aside className="text-white">
+          <IoIosBookmarks className="size-16" />
+          <p>
+            CivilPrep
+            <br />
+            Providing reliable tech since 1992
+          </p>
+        </aside>
+        <nav className="text-white">
+          <h6 className="footer-title">Social</h6>
+          <div className="grid grid-flow-col gap-4">
+            <a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="fill-current"
+              >
+                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+              </svg>
+            </a>
+            <a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="fill-current"
+              >
+                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+              </svg>
+            </a>
+            <a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="fill-current"
+              >
+                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+              </svg>
+            </a>
+          </div>
+        </nav>
+      </footer>
       {/* Floating CIV.ai Icon */}
       <div className="fixed bottom-8 right-8 z-50">
         <button
